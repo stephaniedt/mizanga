a631bea94d197a2e2a838966edd3f4fffe7d18ed
carolfps <37373110+carolfps@users.noreply.github.com>
2020-01-09 10:11:45 -0300

    Criado Tela Cadastro Inscrito

---
+++ cadast_inscrito.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<?php require('./includes/head.php'); ?>
+
+<body>
+    <div class="container-fluid">
+        <div>
+            <?php require('./includes/navbar.php'); ?>
+        </div> 
+    </div>
+
+    <div class="container">
+        <div class="mb-1 pt-1">
+                <h4 class="display-2">Ficha de Inscrição</h4>
+                <p class="lead font-italic"></p>
+        </div>
+        <form action="" method="post">
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="modalidade">Modalidade</label>
+                    <input type="text" class="form-control" id="modalidade" placeholder="Modalidade">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="input-group col-md-6">
+                    <label for="compMatricula">Comprovante de matrícula</label>
+                    <input type="text" class="form-control" id="compMatricula" placeholder="Comprovante de matrícula" aria-label="Comprovante de matrícula" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                    <button class="btn btn-outline-primary" type="button">Buscar</button>
+                    </div>
+                </div>
+            </div>
+
+            <h5>Dados pessoais</h5>
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label for="cpf">CPF*</label>
+                    <input type="text" class="form-control" id="cpf" placeholder="CPF">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="rg">RG*</label>
+                    <input type="text" class="form-control" id="rg" placeholder="RG">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="emissor">Orgão emissor*</label>
+                    <input type="text" class="form-control" id="emissor" placeholder="Orgão Emissor">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-8">
+                    <label for="nome">Nome*</label>
+                    <input type="text" class="form-control" id="nome" placeholder="Nome">
+                    <small id="passwordHelpBlock" class="form-text text-muted">*Atenção: Confira se seu nome foi digitado corretamente!</small>
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="nascimento">Data nascimento*</label>
+                    <input type="text" class="form-control" id="nascimento" placeholder="DD/MM/AAAA">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="nomeSocial">Nome Social*</label>
+                    <input type="text" class="form-control" id="nomeSocial" placeholder="Nome Social">
+                    <small id="passwordHelpBlock" class="form-text text-muted">Se desejar, inclua neste campo seu nome de uso habitual.</small>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="instituicao">Instituição para crachá</label>
+                    <input type="text" class="form-control" id="instituicao" placeholder="Instituição para crachá">
+                </div>
+            </div>
+            
+            <h5>Formação</h5>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="titulacao">Titulação mais alta*</label>
+                    <select class="custom-select" id="titulacao">
+                        <option selected>Escolha...</option>
+                        <option value="1">Ensino Médio</option>
+                        <option value="2">Graduação</option>
+                        <option value="3">Mestrado</option>
+                        <option value="4">Doutorado</option>
+                        <option value="5">Pós-Doutorado</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="instituicaoTitul">Instituição de titulação*</label>
+                    <input type="text" class="form-control" id="instituicaoTitul" placeholder="Instituição de titulação">
+                </div>
+            </div>
+
+            <h5>Atuação Profissional</h5>
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label for="situacaoProfis">Situação profissional*</label>
+                    <input type="text" class="form-control" id="situacaoProfis" placeholder="Situação profissional">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="filiacao">Filiação institucional</label>
+                    <input type="text" class="form-control" id="filiacao" placeholder="Filiação Insticional">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="cargo">Função institucional/cargo</label>
+                    <input type="text" class="form-control" id="cargo" placeholder="Filiação institucional/cargo">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-12">
+                    <label for="areasAtuacao">Áreas de atuação (informe as áreas de atuação separadas por ponto e vírgula)</label>
+                    <input type="text" class="form-control" id="areasAtuacao" placeholder="Áreas de atuação">
+                </div>
+            </div>
+            
+            <h5>Endereço</h5>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                <label class="form-check-label" for="inlineRadio1">Residência no Brasil</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                <label class="form-check-label" for="inlineRadio2">Residência no Exterior</label>
+            </div>
+
+            <!-- <div class="form-check">
+                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+                <label class="form-check-label" for="exampleRadios1">
+                Residência no Brasil
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+                <label class="form-check-label" for="exampleRadios2">
+                Residência no Exterior
+                </label>
+            </div> -->
+
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label for="cep">CEP*</label>
+                    <input type="text" class="form-control" id="cep" placeholder="CEP">
+                    <a href="">Consulte seu CEP</a>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="logradouro">Logradouro*</label>
+                    <input type="text" class="form-control" id="logradouro" placeholder="Logradouro">
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="numero">Número</label>
+                    <input type="text" class="form-control" id="numero" placeholder="Número">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="complemento">Complemento</label>
+                    <input type="text" class="form-control" id="complemento" placeholder="Complemento">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label for="bairro">Bairro</label>
+                    <input type="text" class="form-control" id="bairro" placeholder="Bairro">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="municipio">Município</label>
+                    <input type="text" class="form-control" id="municipio" placeholder="Município">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="uf">UF*</label>
+                    <select class="custom-select" id="uf">
+                        <option selected>Escolha...</option>
+                        <option value="1">AC</option>
+                        <option value="2">AL</option>
+                        <option value="3">AP</option>
+                        <option value="4">AM</option>
+                        <option value="5">BA</option>
+                        <option value="6"> CE</option>
+                        <option value="7"> DF</option>
+                        <option value="8"> ES</option>
+                        <option value="9"> GO</option>
+                        <option value="10">MA</option>
+                        <option value="11">MG</option>
+                        <option value="12">MT</option>
+                        <option value="13">MS</option>
+                        <option value="14">PA</option>
+                        <option value="15">PB</option>
+                        <option value="16">PR</option>
+                        <option value="17">PE</option>
+                        <option value="18">PI</option>
+                        <option value="19">RJ</option>
+                        <option value="20">RN</option>
+                        <option value="21">RS</option>
+                        <option value="22">RO</option>
+                        <option value="23">RR</option>
+                        <option value="24">SC</option>
+                        <option value="25">SP</option>
+                        <option value="26">SE</option>
+                        <option value="27">TO</option>
+                    </select>
+                </div>
+            </div>
+
+            <h5>Dados de contato</h5>
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label for="telResid">Telefone residencial</label>
+                    <input type="text" class="form-control" id="telResid" placeholder="Telefone residencial">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="telProfis">Telefone profissional</label>
+                    <input type="text" class="form-control" id="telProfis" placeholder="Telefone profissional">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="telCelular">Telefone celular</label>
+                    <input type="text" class="form-control" id="telCelular" placeholder="Telefone celular">
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="email">E-mail*</label>
+                    <input type="text" class="form-control" id="email" placeholder="E-mail">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="confirmEmail">Confirmar e-mail*</label>
+                    <input type="text" class="form-control" id="confirmEmail" placeholder="Confirmar e-mail">
+                </div>
+            </div>
+
+            <h5>Acessibilidade</h5>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    Necessita de recursos de acessibilidade?*
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="acessSim" value="sim">
+                    <label class="form-check-label" for="acessSim">Sim</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="acessNao" value="nao">
+                    <label class="form-check-label" for="acessNao">Não</label>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    Possui alguma deficiência?
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="deficSim" value="sim">
+                    <label class="form-check-label" for="deficSim">Sim</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="deficNao" value="nao">
+                    <label class="form-check-label" for="deficNao">Não</label>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    Faz uso de assistência pessoal e/ou equipamento de tecnologia assistiva?
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="tecAssistSim" value="sim">
+                    <label class="form-check-label" for="tecAssistSim">Sim</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="tecAssistNao" value="nao">
+                    <label class="form-check-label" for="tecAssistNao">Não</label>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    Necessita de recursos de acessibilidade?
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="recursoSim" value="sim">
+                    <label class="form-check-label" for="recursoSim">Sim</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="recursoNao" value="nao">
+                    <label class="form-check-label" for="recursoNao">Não</label>
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-12">
+                    <label for="outrasQuestoes">Utilize este espaço para indicar outras questões, orientações ou necessidades não previstas por nosso formulário</label>
+                    <input type="text" class="form-control" id="outrasQuestoes" placeholder="">
+                </div>
+            </div>
+
+            <h5>Dados complementares</h5>
+            <div class="form-row">
+                <div class="form-group col-md-2">
+                    <label for="genero">Genêro</label>
+                    <input type="text" class="form-control" id="genero" placeholder="Genêro">
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="raca">Raça/Cor*</label>
+                    <select class="custom-select" id="raca">
+                        <option selected>Escolha...</option>
+                        <option value="1">Amarela</option>
+                        <option value="2">Branca</option>
+                        <option value="3">Indígena</option>
+                        <option value="4">Parda</option>
+                        <option value="5">Preta</option>
+                        <option value="6">Não desejo declarar</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-8">
+                    <label for="nomeCertific">Prefere que o nome social seja utilizado na emissão do certificado?*</label>
+                    <select class="custom-select" id="nomeCertific">
+                        <option selected>Escolha...</option>
+                        <option value="1">Sim</option>
+                        <option value="2">Não</option>
+                    </select>
+                </div>
+            </div>
+        </form>
+
+        <button type="submit" class="btn btn-primary">Enviar</button>
+        <button type="button" class="btn btn-primary">Voltar</button>
+    </div>
+
+    <div>
+        <?php require('./includes/footer.php'); ?>
+        <?php require('./includes/script.php'); ?>
+    </div>
+    
+</body>
+</html>
