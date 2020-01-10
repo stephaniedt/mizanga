ce0eb2c9f354378aa169eeb1f2c44fb4978f1548
carolfps <37373110+carolfps@users.noreply.github.com>
2020-01-10 09:41:19 -0300

    inclusao de conteudo na pagina

--- incluir_parecerista.php
+++ incluir_parecerista.php
@@ -15,6 +15,105 @@
         </div>
     </div>
 
+    <div class="container">
+        <form action="" method="post">
+            <div class="my-3">
+                <div class="form-row">
+                        <div class="form-group col-md-4">
+                            <label for="cpf">CPF*</label>
+                            <input type="text" class="form-control" id="cpf" placeholder="Nome">
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="nome">Nome*</label>
+                            <input type="text" class="form-control" id="nome" placeholder="Nome">
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="email">E-mail*</label>
+                            <input type="text" class="form-control" id="email" placeholder="E-mail">
+                        </div>
+                </div>
+            </div>
+
+            <div class="my-3">
+                <h5>Eixos teimáticos</h5>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        Eixo 01 - Lorem ipsum dolor sit amet consectetur
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="eixoUmSim" value="sim">
+                        <label class="form-check-label" for="eixoUmSim">Sim</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="eixoUmNao" value="nao">
+                        <label class="form-check-label" for="eixoUmNao">Não</label>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        Eixo 02 - Lorem ipsum dolor sit amet consectetur
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="eixoDoisSim" value="sim">
+                        <label class="form-check-label" for="eixoDoisSim">Sim</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="eixoDoisNao" value="nao">
+                        <label class="form-check-label" for="eixoDoisNao">Não</label>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        Eixo 03 - Lorem ipsum dolor sit amet consectetur
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="eixoTresSim" value="sim">
+                        <label class="form-check-label" for="eixoTresSim">Sim</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="eixoTresNao" value="nao">
+                        <label class="form-check-label" for="eixoTresNao">Não</label>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        Eixo 04 - Lorem ipsum dolor sit amet consectetur
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="eixoQuatroSim" value="sim">
+                        <label class="form-check-label" for="eixoQuatroSim">Sim</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="eixoQuatroNao" value="nao">
+                        <label class="form-check-label" for="eixoQuatroNao">Não</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="my-3">
+                <h5>Após salvar:</h5>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="opcao1" value="option1">
+                    <label class="form-check-label" for="opcao1">
+                        Enviar carta convite
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="opcao2" value="option2">
+                    <label class="form-check-label" for="opcao2">
+                        Cadastrar novo
+                    </label>
+                </div>
+            </div>
+        </form>
+
+        <div class="my-4">
+            <button type="submit" class="btn btn-primary" style="width: 10%">Enviar</button>
+            <button type="button" class="btn btn-primary" style="width: 10%">Voltar</button>
+        </div>
+
+    </div>
+
     <div>
         <?php require('./includes/footer.php'); ?>
         <?php require('./includes/script.php'); ?>
