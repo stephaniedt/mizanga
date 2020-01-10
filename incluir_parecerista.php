3fbeb9adc7948f08528f037b029bb92923426de0
carolfps <37373110+carolfps@users.noreply.github.com>
2020-01-10 09:09:25 -0300

    Criado tela incluir parecerista

---
+++ incluir_parecerista.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<?php require('./includes/head.php'); ?>
+<body>
+    <div class="container-fluid">
+        <div>
+            <?php require('./includes/navbar.php'); ?>
+        </div> 
+    </div>
+
+    <div class="container">
+    <div class="mb-1 pt-1">
+                <h4 class="display-2">Incluir Parecerista</h4>
+                <p class="lead font-italic"></p>
+        </div>
+    </div>
+
+    <div>
+        <?php require('./includes/footer.php'); ?>
+        <?php require('./includes/script.php'); ?>
+    </div>
+    
+</body>
+</html>
