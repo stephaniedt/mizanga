d3d45e0fce86a02ad090fe8d4b2b9be7901d7c53
carolfps <37373110+carolfps@users.noreply.github.com>
2020-01-10 10:24:40 -0300

    atualizado conteudo da pagina

--- area_inscrito.php
+++ area_inscrito.php
@@ -2,6 +2,133 @@
 <html lang="pt-br">
 <?php require('./includes/head.php'); ?>
 <body>
-    
+    <div class="container-fluid">
+        <div>
+            <?php require('./includes/navbar.php'); ?>
+        </div> 
+    </div>
+
+    <div class="container">
+        <div class="mb-1 pt-1">
+            <h4 class="display-2">Área do inscrito</h4>
+            <p class="lead font-italic"></p>
+        </div>
+
+        <div class="my-3">
+            <h5>Certificados</h5>
+            <ul>
+                <li>Certificado de apresentação de pôster <a href="">(Clique aqui para imprimir)</a></li>
+                    <ul style="list-style-type:none">
+                        <li> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                    </ul>
+                <li>Certitificado de participação <a href="">(Clique aqui para imprimir)</a></li>
+                    <ul style="list-style-type:none">
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                    </ul>
+            </ul>
+        </div>
+        <hr>
+
+        <div class="my-3">
+            <h5>Opções</h5>
+            <ul style="list-style-type:none">
+                <li><a href="">Alterar dados de inscrição</a></li>
+                <li><a href="">Alterar modalidade de participação</a></li>
+                <li><a href="">Inscrever-se em atividade</a></li>
+            </ul>
+        </div>
+        <hr>
+
+        <div class="my-3">
+            <h5>Dados da inscrição</h5>
+            <p>Código da Inscrição: XXXXXX <br>
+            Modalidade: Estudante não associado <br>
+            Data de inscrição: 20/11/2019 - 10:20 <br>
+            Pedido XXXXXX - R$200,00</p>
+        </div>
+        <hr>
+
+        <div class="my-3">
+            <h5>Dados pessoais</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus montes, posuere dis elit arcu. Enim viverra ornare integer rhoncus facilisis ipsum. Ornare malesuada et neque, eu a, mauris vulputate dolor, sagittis. Aliquam, elit id vitae placerat eget erat elit tortor eu. Libero, massa vitae, senectus ullamcorper. Cursus sit in viverra convallis volutpat. Nunc velit vitae sociis amet rhoncus, egestas ut a sed.</p>
+        </div>
+        <hr>
+
+        <div class="my-3">
+            <h5>Atuação profissional</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus montes, posuere dis elit arcu. Enim viverra ornare integer rhoncus facilisis ipsum. Ornare malesuada et neque, eu a, mauris vulputate dolor, sagittis. Aliquam, elit id vitae placerat eget erat elit tortor eu. Libero, massa vitae, senectus ullamcorper. Cursus sit in viverra convallis volutpat. Nunc velit vitae sociis amet rhoncus, egestas ut a sed.</p>
+        </div>
+        <hr>
+
+        <div class="my-3">
+            <h5>Endereço</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus montes, posuere dis elit arcu. Enim viverra ornare integer rhoncus facilisis ipsum. Ornare malesuada et neque, eu a, mauris vulputate dolor, sagittis. Aliquam, elit id vitae placerat eget erat elit tortor eu. Libero, massa vitae, senectus ullamcorper. Cursus sit in viverra convallis volutpat. Nunc velit vitae sociis amet rhoncus, egestas ut a sed.</p>
+        </div>
+        <hr>
+
+        <div class="my-3">
+            <h5>Dados de contato</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus montes, posuere dis elit arcu. Enim viverra ornare integer rhoncus facilisis ipsum. Ornare malesuada et neque, eu a, mauris vulputate dolor, sagittis. Aliquam, elit id vitae placerat eget erat elit tortor eu. Libero, massa vitae, senectus ullamcorper. Cursus sit in viverra convallis volutpat. Nunc velit vitae sociis amet rhoncus, egestas ut a sed.</p>
+        </div>
+        <hr>
+
+        <div class="my-3">
+            <h5>Dados complementares</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus montes, posuere dis elit arcu. Enim viverra ornare integer rhoncus facilisis ipsum. Ornare malesuada et neque, eu a, mauris vulputate dolor, sagittis. Aliquam, elit id vitae placerat eget erat elit tortor eu. Libero, massa vitae, senectus ullamcorper. Cursus sit in viverra convallis volutpat. Nunc velit vitae sociis amet rhoncus, egestas ut a sed.</p>
+        </div>
+        <hr>
+        
+        <div class="my-3">
+            <h5>Trabalhos submetidos</h5>
+            <p> 
+                Proposta para Grupos de Trabalho <br>
+                Código do trabalho: XXXXXX <br>
+                Resumo: <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus montes, posuere dis elit arcu. Enim viverra ornare integer rhoncus facilisis ipsum. Ornare malesuada et neque, eu a, mauris vulputate dolor, sagittis. Aliquam, elit id vitae placerat eget erat elit tortor eu. Libero, massa vitae, senectus ullamcorper. Cursus sit in viverra convallis volutpat. Nunc velit vitae sociis amet rhoncus, egestas ut a sed.
+                Palavras-chave: Lorem ipsum, dolor sit amet, consectetur. <br>
+                Eixo temático: Eixo 01 - Lorem ipsum dolor sit amet, consectetur <br>
+                Nome dos participantes e Filiação institucional:Lorem Ipsum Dolor - USP <br>
+                Sit Amet Consectetur - Unicamp <br>
+                Viverra Convalis - USP <br>
+                Áreas: <br>
+                Alocado: Eixo 01 - Lorem ipsum dolor sit amet, consectetur <br>
+                Sessão: XX <br>
+                Resultado da avaliação: Aprovado
+            </p>
+            <div class="my-2">
+            <button type="button" class="btn btn-outline-primary" style="width: 250px">Carta de aceite</button>
+            </div>
+        </div>
+        <hr>
+
+        <div class="mt-3 mb-5">
+            <h5>Participação em outras propostas</h5>
+            <p> 
+                Proposta para Grupos de Trabalho <br>
+                Código do trabalho: XXXXXX <br>
+                Resumo: <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus montes, posuere dis elit arcu. Enim viverra ornare integer rhoncus facilisis ipsum. Ornare malesuada et neque, eu a, mauris vulputate dolor, sagittis. Aliquam, elit id vitae placerat eget erat elit tortor eu. Libero, massa vitae, senectus ullamcorper. Cursus sit in viverra convallis volutpat. Nunc velit vitae sociis amet rhoncus, egestas ut a sed.
+                Palavras-chave: Lorem ipsum, dolor sit amet, consectetur. <br>
+                Eixo temático: Eixo 01 - Lorem ipsum dolor sit amet, consectetur <br>
+                Nome dos participantes e Filiação institucional:Lorem Ipsum Dolor - USP <br>
+                Sit Amet Consectetur - Unicamp <br>
+                Viverra Convalis - USP <br>
+                Áreas: <br>
+                Alocado: Eixo 01 - Lorem ipsum dolor sit amet, consectetur <br>
+                Sessão: XX <br>
+                Resultado da avaliação: Aprovado
+            </p>
+            <div class="my-2">
+                <button type="button" class="btn btn-outline-primary" style="width: 250px">Carta de avaliação</button>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div>
+        <?php require('./includes/footer.php'); ?>
+        <?php require('./includes/script.php'); ?>
+    </div>
 </body>
 </html>
