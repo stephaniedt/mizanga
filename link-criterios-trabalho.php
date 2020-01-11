836bf3461fe206b71eddf841fa02bff1a5f62af6
Stephanie Tavares <stephanie.diast@hotmail.com>
2020-01-11 14:01:44 -0300

    criação de pagina link-criterios-trabalho.php

---
+++ link-criterios-trabalho.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require('./includes/head.php'); ?>
+<body>
+    <div class="container-fluid">
+        <div>
+            <?php require('./includes/navbar.php'); ?>
+        </div>
+    </div>
+    <main>
+        <div class="container">
+           
+            <h1 class="display-2 mb-3">Critérios para inscrição de trabalhos</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus vero praesentium sed nemo at quo eligendi, officiis, magnam provident natus, velit eos cupiditate impedit unde esse dicta consectetur aliquid possimus!
+            Doloremque facere inventore mollitia blanditiis repellendus similique repudiandae quia dignissimos voluptatibus sapiente, incidunt architecto corrupti ullam ipsam earum accusamus quibusdam nesciunt! Saepe ea maiores recusandae voluptate eius exercitationem sint quod?
+            Architecto sapiente alias a, odio recusandae, temporibus aliquam voluptatem perferendis officia dolore amet consequatur dolorum maxime explicabo praesentium, eum quis perspiciatis aspernatur. Magni amet molestiae aspernatur vero non temporibus deleniti?</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus vero praesentium sed nemo at quo eligendi, officiis, magnam provident natus, velit eos cupiditate impedit unde esse dicta consectetur aliquid possimus!
+            Doloremque facere inventore mollitia blanditiis repellendus similique repudiandae quia dignissimos voluptatibus sapiente, incidunt architecto corrupti ullam ipsam earum accusamus quibusdam nesciunt! Saepe ea maiores recusandae voluptate eius exercitationem sint quod?
+            Architecto sapiente alias a, odio recusandae, temporibus aliquam voluptatem perferendis officia dolore amet consequatur dolorum maxime explicabo praesentium, eum quis perspiciatis aspernatur. Magni amet molestiae aspernatur vero non temporibus deleniti?</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus vero praesentium sed nemo at quo eligendi, officiis, magnam provident natus, velit eos cupiditate impedit unde esse dicta consectetur aliquid possimus!
+            Doloremque facere inventore mollitia blanditiis repellendus similique repudiandae quia dignissimos voluptatibus sapiente, incidunt architecto corrupti ullam ipsam earum accusamus quibusdam nesciunt! Saepe ea maiores recusandae voluptate eius exercitationem sint quod?
+            Architecto sapiente alias a, odio recusandae, temporibus aliquam voluptatem perferendis officia dolore amet consequatur dolorum maxime explicabo praesentium, eum quis perspiciatis aspernatur. Magni amet molestiae aspernatur vero non temporibus deleniti?</p>   
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus vero praesentium sed nemo at quo eligendi, officiis, magnam provident natus, velit eos cupiditate impedit unde esse dicta consectetur aliquid possimus!
+            Doloremque facere inventore mollitia blanditiis repellendus similique repudiandae quia dignissimos voluptatibus sapiente, incidunt architecto corrupti ullam ipsam earum accusamus quibusdam nesciunt! Saepe ea maiores recusandae voluptate eius exercitationem sint quod?
+            Architecto sapiente alias a, odio recusandae, temporibus aliquam voluptatem perferendis officia dolore amet consequatur dolorum maxime explicabo praesentium, eum quis perspiciatis aspernatur. Magni amet molestiae aspernatur vero non temporibus deleniti?</p>   
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus vero praesentium sed nemo at quo eligendi, officiis, magnam provident natus, velit eos cupiditate impedit unde esse dicta consectetur aliquid possimus!
+            Doloremque facere inventore mollitia blanditiis repellendus similique repudiandae quia dignissimos voluptatibus sapiente, incidunt architecto corrupti ullam ipsam earum accusamus quibusdam nesciunt! Saepe ea maiores recusandae voluptate eius exercitationem sint quod?
+            Architecto sapiente alias a, odio recusandae, temporibus aliquam voluptatem perferendis officia dolore amet consequatur dolorum maxime explicabo praesentium, eum quis perspiciatis aspernatur. Magni amet molestiae aspernatur vero non temporibus deleniti?</p>   
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus vero praesentium sed nemo at quo eligendi, officiis, magnam provident natus, velit eos cupiditate impedit unde esse dicta consectetur aliquid possimus!
+            Doloremque facere inventore mollitia blanditiis repellendus similique repudiandae quia dignissimos voluptatibus sapiente, incidunt architecto corrupti ullam ipsam earum accusamus quibusdam nesciunt! Saepe ea maiores recusandae voluptate eius exercitationem sint quod?
+            Architecto sapiente alias a, odio recusandae, temporibus aliquam voluptatem perferendis officia dolore amet consequatur dolorum maxime explicabo praesentium, eum quis perspiciatis aspernatur. Magni amet molestiae aspernatur vero non temporibus deleniti?</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus vero praesentium sed nemo at quo eligendi, officiis, magnam provident natus, velit eos cupiditate impedit unde esse dicta consectetur aliquid possimus!
+            Doloremque facere inventore mollitia blanditiis repellendus similique repudiandae quia dignissimos voluptatibus sapiente, incidunt architecto corrupti ullam ipsam earum accusamus quibusdam nesciunt! Saepe ea maiores recusandae voluptate eius exercitationem sint quod?
+            Architecto sapiente alias a, odio recusandae, temporibus aliquam voluptatem perferendis officia dolore amet consequatur dolorum maxime explicabo praesentium, eum quis perspiciatis aspernatur. Magni amet molestiae aspernatur vero non temporibus deleniti?</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus vero praesentium sed nemo at quo eligendi, officiis, magnam provident natus, velit eos cupiditate impedit unde esse dicta consectetur aliquid possimus!
+            Doloremque facere inventore mollitia blanditiis repellendus similique repudiandae quia dignissimos voluptatibus sapiente, incidunt architecto corrupti ullam ipsam earum accusamus quibusdam nesciunt! Saepe ea maiores recusandae voluptate eius exercitationem sint quod?
+            Architecto sapiente alias a, odio recusandae, temporibus aliquam voluptatem perferendis officia dolore amet consequatur dolorum maxime explicabo praesentium, eum quis perspiciatis aspernatur. Magni amet molestiae aspernatur vero non temporibus deleniti?</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus vero praesentium sed nemo at quo eligendi, officiis, magnam provident natus, velit eos cupiditate impedit unde esse dicta consectetur aliquid possimus!
+            Doloremque facere inventore mollitia blanditiis repellendus similique repudiandae quia dignissimos voluptatibus sapiente, incidunt architecto corrupti ullam ipsam earum accusamus quibusdam nesciunt! Saepe ea maiores recusandae voluptate eius exercitationem sint quod?
+            Architecto sapiente alias a, odio recusandae, temporibus aliquam voluptatem perferendis officia dolore amet consequatur dolorum maxime explicabo praesentium, eum quis perspiciatis aspernatur. Magni amet molestiae aspernatur vero non temporibus deleniti?</p>
+
+
+    </main>
+    <div>
+        <?php require('./includes/footer.php'); ?>
+        <?php require('./includes/script.php'); ?>
+    </div>
+</body>
+</html>
