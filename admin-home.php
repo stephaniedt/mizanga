23a38c08b4a4f80a7a1822d85c2c86fed08b08b2
macris-simoes <macris.simoes@gmail.com>
2020-02-02 17:34:38 -0300

    alterações no admin-home.php

--- admin-home.php
+++ admin-home.php
@@ -9,21 +9,19 @@
       <aside class="col-md-3 col-sm-12 m-2">
         <img style="width:100%" src="./img/mizangueras.gif" alt="">
         <h1 class= "text-center">1º Encontro das mizangueiras</h1>
-        <p class="lead text-center">de 28/10/2019 a 28/04/2020 <br> Veja como estão as coisas!</p>
-        
+        <p class="lead text-center">de 28/10/2019 a 28/04/2020 <br> Veja como estão as coisas!</p> 
       </aside>
 
       <!-- início card deck -->
       <section class="card-group col-md-9 row mt-2 flex-wrap">
-
-        <!-- início card pareceristas -->
-        <div class="card col-md-4 ">
-            <h5 class="card-title">Pareceristas</h5>
-            <!-- aqui tem php hein -->
-            <p class="card-text">Pareceristas convidados: 1234 </p>
-            <p class="card-text">Pareceristas cadastrados: 0123 </p>
+      
+        <!-- início card Inscritos -->
+        <div class="card col-md-4 rounded-right">
+          <h5 class="card-title">Inscritos</h5>
+          <!-- aqui tem php hein -->
+          <p class="card-text">Inscritos: 123456 </p>
         </div>
-        <!-- fim card pareceristas -->
+        <!-- fim card Inscritos -->
 
         <!-- início card trabalhos -->
         <div class="card col-md-4 ">
@@ -35,33 +33,33 @@
         </div>
         <!-- fim card trabalhos -->
 
-        <!-- início card Inscritos -->
-        <div class="card col-md-4 rounded-right">
-          <h5 class="card-title">Inscritos</h5>
-          <!-- aqui tem php hein -->
-          <p class="card-text">Inscritos: 123456 </p>
+        <!-- início card pareceristas -->
+        <div class="card col-md-4 ">
+            <h5 class="card-title">Pareceristas</h5>
+            <!-- aqui tem php hein -->
+            <p class="card-text">Pareceristas convidados: 1234 </p>
+            <p class="card-text">Pareceristas cadastrados: 0123 </p>
         </div>
-        <!-- fim card Inscritos -->
+        <!-- fim card pareceristas -->
 
+        <!-- início card maior (prazos) -->
         <div class="col-md-12 border rounded mt-2">
-          <div>
             <h5 class="card-title">Prazos</h5>
             <!-- aqui tem php hein -->
             <p class="card-text">Abertura de inscrições: 25/01/2020 </p>
-          </div>
+            <p class="card-text">Abertura para trabalhos: 25/01/2020 </p>
+            <p class="card-text">Dilatação de prazo (não divulgar esta informação): 25/01/2020 </p>
         </div>
-
+        <!-- fim card maior (prazos) -->
 
       </section>
       <!-- fim card deck -->
-
     
-  </div>
-    <!-- fim flex-row -->
+    </div>
+    <!-- fim div row -->
 
   </div>
   <!-- fim div container -->
 
 </body>
-
 </html>
