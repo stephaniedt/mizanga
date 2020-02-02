04454bad7eb45198d4e5877e033f843eebc58222
macris-simoes <macris.simoes@gmail.com>
2020-02-02 17:27:34 -0300

    arrumando a home do admin, acho que devemos tentar fazer em todas as homes (admin, inscrito, parecerista)

--- admin-home.php
+++ admin-home.php
@@ -5,7 +5,7 @@
   <div class="container">
 
     <!-- Cabeçalho -->
-    <div class="row justify-content-md-center">
+    <div class="row">
       <aside class="col-md-3 col-sm-12 m-2">
         <img style="width:100%" src="./img/mizangueras.gif" alt="">
         <h1 class= "text-center">1º Encontro das mizangueiras</h1>
@@ -14,46 +14,47 @@
       </aside>
 
       <!-- início card deck -->
-      <section class="col-md-8 m-2">
+      <section class="card-group col-md-9 row mt-2 flex-wrap">
 
         <!-- início card pareceristas -->
-        <div class="card m-1 col-md-12">
-          <div class="card-body row">
-              <div class="col-md-8">
-                <h5 class="card-title">Pareceristas</h5>
-                <!-- aqui tem php hein -->
-                <p class="card-text">Pareceristas convidados: 1234 </p>
-                <p class="card-text">Pareceristas cadastrados: 0123 </p>
-              </div>
-          </div>
+        <div class="card col-md-4 ">
+            <h5 class="card-title">Pareceristas</h5>
+            <!-- aqui tem php hein -->
+            <p class="card-text">Pareceristas convidados: 1234 </p>
+            <p class="card-text">Pareceristas cadastrados: 0123 </p>
         </div>
         <!-- fim card pareceristas -->
 
         <!-- início card trabalhos -->
-        <div class="card m-1 col-md-12">
-            <div class="card-body">
-            <h5 class="card-title">Trabalhos</h5>
-            <!-- aqui tem php hein -->
-            <p class="card-text">Trabalhos inscritos: 1234 </p>
-            <p class="card-text">Trabalhos avaliados: 0123 </p>
-            <p class="card-text">Trabalhos aguardando avaliação: 0123 </p>
-          </div>
+        <div class="card col-md-4 ">
+          <h5 class="card-title">Trabalhos</h5>
+          <!-- aqui tem php hein -->
+          <p class="card-text">Trabalhos inscritos: 1234 </p>
+          <p class="card-text">Trabalhos avaliados: 0123 </p>
+          <p class="card-text">Trabalhos aguardando avaliação: 0123 </p>
         </div>
         <!-- fim card trabalhos -->
 
         <!-- início card Inscritos -->
-        <div class="card m-1 col-md-12">
-            <div class="card-body">
-            <h5 class="card-title">Inscritos</h5>
-            <!-- aqui tem php hein -->
-            <p class="card-text">Inscritos: 123456 </p>
-            
-          </div>
+        <div class="card col-md-4 rounded-right">
+          <h5 class="card-title">Inscritos</h5>
+          <!-- aqui tem php hein -->
+          <p class="card-text">Inscritos: 123456 </p>
         </div>
         <!-- fim card Inscritos -->
-        
+
+        <div class="col-md-12 border rounded mt-2">
+          <div>
+            <h5 class="card-title">Prazos</h5>
+            <!-- aqui tem php hein -->
+            <p class="card-text">Abertura de inscrições: 25/01/2020 </p>
+          </div>
+        </div>
+
+
       </section>
       <!-- fim card deck -->
+
     
   </div>
     <!-- fim flex-row -->
