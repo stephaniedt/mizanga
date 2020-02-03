3b84a38b251bcc5c25da05c2a0fc39c6a24afaf2
carolfps <37373110+carolfps@users.noreply.github.com>
2020-02-03 16:01:11 -0300

    footer no fim da pagina

--- login.php
+++ login.php
@@ -4,41 +4,45 @@
 
 <body>
 
-  <div class="container-fluid">
-    <div>
-      <?php require('./includes/navbar.php'); ?>
-    </div> 
-  </div>
-   
-  <main class="container">
+	<div class="container-fluid">
+		<div>
+			<?php require('./includes/navbar.php'); ?>
+		</div>
+	</div>
 
-  <!-- início config -->
+	<main class="container">
 
-  <!-- início do login -->      
+		<!-- início config -->
+
+		<!-- início do login -->
 		<div class="row">
 			<div class="col-6 offset-3">
-				
+
 				<form method="post">
-				
-				<div class="form-group">
-					<label for="nome">Usuário</label>
-					<input type="text" class="form-control <?= $nomeOK ? '' : 'is-invalid' ; ?>" id="usuario" name="usuario" placeholder="usuario" required>	
-					<div class="invalid-feedback">Digite um usuário válido</div>
-				</div>
-				
-                <div class="form-group">
-					<label for="senha">Senha</label>
-					<input type="password" class="form-control" id="senha" name="senha" placeholder="senha" required>	
-				</div class="col-md-1 d-flex align-items-center">
+
+					<div class="form-group">
+						<label for="nome">Usuário</label>
+						<input type="text" class="form-control <?= $nomeOK ? '' : 'is-invalid'; ?>" id="usuario" name="usuario" placeholder="usuario" required>
+						<div class="invalid-feedback">Digite um usuário válido</div>
+					</div>
+
+					<div class="form-group">
+						<label for="senha">Senha</label>
+						<input type="password" class="form-control" id="senha" name="senha" placeholder="senha" required>
+					</div>
 					<button class="btn btn-primary" type="submit">Login</button>
 				</form>
 			</div>
-           
+
 		</div>
-  </main>
-    <div>
-    <?php require('./includes/footer.php'); ?>
-    <?php require('./includes/script.php'); ?>
-  </div> 
+	</main>
+	<div style="height: 270px;">
+
+	</div>
+	<div>
+		<?php require('./includes/footer.php'); ?>
+		<?php require('./includes/script.php'); ?>
+	</div>
 </body>
+
 </html>
